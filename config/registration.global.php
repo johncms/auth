099db2984dad0a67e16a87137e74ce580cf5fc74
simba77 <simba7@inbox.ru>
2023-05-27 13:27:57 +0300

    The Auth module has been moved to a separate repository

---
+++ config/registration.global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'registration' => [
+        'closed'             => false,
+        'moderation'         => false,
+        'show_email'         => true,
+        'email_required'     => true,
+        'email_confirmation' => true,
+    ],
+];
