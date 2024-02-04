c97e98df37828e582a0e99efcc5aebdfe71f2865
simba77 <simba7@inbox.ru>
2024-02-04 20:06:35 +0300

    Removed optional last slashes because now it handled by router

--- config/routes.php
+++ config/routes.php
@@ -12,36 +12,37 @@ use Johncms\Users\Middlewares\UnauthorizedUserMiddleware;
 
 return function (RouteCollection $router) {
     // Login routes
-    $router->get('/login[/]', [LoginController::class, 'index'])
-        ->addMiddleware([UnauthorizedUserMiddleware::class])
+    $router->get('/login/', [LoginController::class, 'index'])
+        ->addMiddleware(UnauthorizedUserMiddleware::class)
         ->setName('login.index');
-    $router->post('/login/authorize[/]', [LoginController::class, 'authorize'])
-        ->addMiddleware([UnauthorizedUserMiddleware::class])
+    $router->post('/login/authorize/', [LoginController::class, 'authorize'])
+        ->addMiddleware(UnauthorizedUserMiddleware::class)
         ->setName('login.authorize');
 
     // Registration routes
-    $router->get('/registration[/]', [RegistrationController::class, 'index'])
-        ->addMiddleware([UnauthorizedUserMiddleware::class, RegistrationClosedMiddleware::class])
+    $router->get('/registration/', [RegistrationController::class, 'index'])
+        ->addMiddleware(UnauthorizedUserMiddleware::class)
+        ->addMiddleware(RegistrationClosedMiddleware::class)
         ->setName('registration.index');
 
     $router->group('/registration', function (RouteCollection $route) {
-        $route->post('/store[/]', [RegistrationController::class, 'store'])
+        $route->post('/store/', [RegistrationController::class, 'store'])
             ->addMiddleware(RegistrationClosedMiddleware::class)
             ->setName('registration.store');
 
-        $route->get('/confirm-email[/]', [RegistrationController::class, 'confirmEmail'])
+        $route->get('/confirm-email/', [RegistrationController::class, 'confirmEmail'])
             ->addMiddleware(RegistrationClosedMiddleware::class)
             ->setName('registration.confirmEmail');
 
-        $route->get('/closed[/]', [RegistrationController::class, 'registrationClosed'])->setName('registration.closed');
+        $route->get('/closed/', [RegistrationController::class, 'registrationClosed'])->setName('registration.closed');
     })->addMiddleware(UnauthorizedUserMiddleware::class);
 
     // Logout routes
-    $router->get('/logout[/]', [LogoutController::class, 'index'])
-        ->addMiddleware([AuthorizedUserMiddleware::class])
+    $router->get('/logout/', [LogoutController::class, 'index'])
+        ->addMiddleware(AuthorizedUserMiddleware::class)
         ->setName('logout.index');
 
-    $router->post('/logout/confirm[/]', [LogoutController::class, 'confirm'])
+    $router->post('/logout/confirm/', [LogoutController::class, 'confirm'])
         ->addMiddleware(AuthorizedUserMiddleware::class)
         ->setName('logout.confirm');
 };
